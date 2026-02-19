27538c09e2980f533fce2e83f694e021777bd8ec
Homfay-0903 <2790165595@qq.com>
2026-02-19 11:31:32 +0800

    Ques-13

---
+++ 13(53).js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    if (!nums.length) {
+        return 0
+    }
+
+    let curSum = nums[0]
+    let maxSum = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+        curSum = Math.max(nums[i], curSum + nums[i])
+        maxSum = Math.max(curSum, maxSum)
+    }
+
+    return maxSum
+};
