b4c4166572632ffb165bddeedb7ff4a1166e7629
Homfay-0903 <2790165595@qq.com>
2026-03-19 19:42:11 +0800

    Ques-43

---
+++ 43(230).js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+    const res = []
+
+    const traverse = (node) => {
+        if (!node) {
+            return
+        }
+
+        traverse(node.left)
+        res.push(node.val)
+        traverse(node.right)
+    }
+
+    traverse(root)
+
+    return res[k - 1]
+};
