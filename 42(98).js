5d5ca08462c59e37e48081e777e368dad5e39100
Homfay-0903 <2790165595@qq.com>
2026-03-19 10:51:43 +0800

    Ques-42

---
+++ 42(98).js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+    const helper = (node, lower, upper) => {
+        if (!node) {
+            return true
+        }
+
+        if (node.val <= lower || node.val >= upper) {
+            return false
+        }
+
+        if (!helper(node.left, lower, node.val)) {
+            return false
+        }
+
+        if (!helper(node.right, node.val, upper)) {
+            return false
+        }
+
+        return true
+    }
+
+    return helper(root, -Infinity, Infinity)
+};
