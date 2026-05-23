b5973cae14edd8fea367f84feae9004d0aaad38e
Homfay-0903 <2790165595@qq.com>
2026-05-23 11:20:39 +0800

    Ques-87

---
+++ 87(416).js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function (nums) {
+    const sum = nums.reduce((acc, num) => (acc + num), 0)
+    if (sum % 2) {
+        return false
+    }
+
+    const target = sum / 2
+    const dp = new Array(target + 1).fill(false)
+    dp[0] = true
+
+    for (const num of nums) {
+        for (let j = target; j >= num; j--) {
+            dp[j] = dp[j] || dp[j - num]
+        }
+
+        if (dp[target]) {
+            return true
+        }
+    }
+
+    return dp[target]
+};
