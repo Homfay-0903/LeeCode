49574d61a4d325692e1fe79e7160f48d0d5ba7da
Homfay-0903 <2790165595@qq.com>
2026-05-24 12:25:39 +0800

    Ques-98

---
+++ 98(287).js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+    const map = new Map()
+
+    for (const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1)
+
+        if (map.get(num) > 1) {
+            return num
+        }
+    }
+};
