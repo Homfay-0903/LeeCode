bb34452b7edb23861da719ae7869c5ba6f887373
Homfay-0903 <2790165595@qq.com>
2026-05-25 12:09:45 +0800

    Ques-3

---
+++ 哈希/3(128).js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+/*var longestConsecutive = function (nums) {
+    const len = nums.length
+    if (!len) {
+        return 0
+    }
+
+    let maxLen = 1
+    let curLen = 1
+
+    nums.sort((a, b) => a - b)
+
+    for (let i = 1; i < len; i++) {
+        if (nums[i] === nums[i - 1]) {
+            continue
+        } else if (nums[i] - nums[i - 1] === 1) {
+            curLen++
+            maxLen = Math.max(maxLen, curLen)
+        } else {
+            curLen = 1
+        }
+    }s
+
+    return maxLen
+};*/
+
+
+var longestConsecutive = function (nums) {
+    const set = new Set(nums)
+    let maxLen = 0
+
+    for (const num of set) {
+        if (!set.has(num - 1)) {
+            let curNum = num
+            let curLen = 1
+
+            while (set.has(curNum + 1)) {
+                curNum++
+                curLen++
+            }
+
+            maxLen = Math.max(maxLen, curLen)
+        }
+    }
+
+    return maxLen
+};
