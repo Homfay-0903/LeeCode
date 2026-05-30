3132339623fe7aac09e18e526e9d3006ce883dd9
Homfay-0903 <2790165595@qq.com>
2026-05-30 19:19:58 +0800

    Ques-20

---
+++ 矩阵/20(48).js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+    const n = matrix.length
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        let left = 0, right = n - 1
+
+        while (left < right) {
+            [matrix[i][left], matrix[i][right]] = [matrix[i][right], matrix[i][left]]
+            left++
+            right--
+        }
+    }
+};
