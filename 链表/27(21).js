ba1e66115c958505ec6b202d762f15a9dd2b23df
Homfay-0903 <2790165595@qq.com>
2026-06-02 10:32:59 +0800

    Ques-27

---
+++ 链表/27(21).js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    const virtualNode = new ListNode(null)
+    let curNode = virtualNode
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            curNode.next = list1
+            list1 = list1.next
+        } else {
+            curNode.next = list2
+            list2 = list2.next
+        }
+
+        curNode = curNode.next
+    }
+
+    curNode.next = list1 ? list1 : list2
+
+    return virtualNode.next
+};
