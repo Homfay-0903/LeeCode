26deea21d91d4314a7d8390cb4c21ed9c8ef1852
Homfay-0903 <2790165595@qq.com>
2026-06-03 11:28:52 +0800

    Ques-31

---
+++ 链表/31(25).js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function (head, k) {
+    if (!head.next || k === 1) {
+        return head
+    }
+
+    const virtualNode = new ListNode(null)
+    let pre1 = virtualNode
+    virtualNode.next = head
+
+    while (true) {
+        let tail = pre1
+
+        for (let i = 0; i < k; i++) {
+            tail = tail.next
+
+            if (!tail) {
+                return virtualNode.next
+            }
+        }
+
+        let pre2 = tail.next
+        let startNode = pre1.next
+        let curNode = startNode
+
+        for (let i = 0; i < k; i++) {
+            const next = curNode.next
+            curNode.next = pre2
+            pre2 = curNode
+            curNode = next
+        }
+
+        pre1.next = pre2
+        pre1 = startNode
+    }
+};
