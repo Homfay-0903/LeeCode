f2bddb894d2259e9c8772689b1fb0d97ec16b8a4
Homfay-0903 <2790165595@qq.com>
2026-06-22 12:25:09 +0800

    Top-15

---
+++ Top-150/数组-字符串/15(135).js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function (ratings) {
+    const n = ratings.length
+    const candyCount = new Array(n).fill(1)
+
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candyCount[i] = candyCount[i - 1] + 1
+        }
+    }
+    for (let i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candyCount[i] = Math.max(candyCount[i], candyCount[i + 1] + 1)
+        }
+    }
+
+    return candyCount.reduce((acc, val) => (acc + val), 0)
+};
