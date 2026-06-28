9c0f5309de2b89fae180ea0e4b97090d8af49de0
Homfay-0903 <2790165595@qq.com>
2026-06-28 14:28:40 +0800

    Top-35

---
+++ Top-150/矩阵/35(54).js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {
+    const res = []
+    const m = matrix.length
+    const n = matrix[0].length
+
+    let left = 0, right = n - 1
+    let top = 0, bottom = m - 1
+
+    while (left <= right && top <= bottom) {
+        for (let i = left; i <= right; i++) {
+            res.push(matrix[top][i])
+        }
+        top++
+
+        for (let i = top; i <= bottom; i++) {
+            res.push(matrix[i][right])
+        }
+        right--
+
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                res.push(matrix[bottom][i])
+            }
+            bottom--
+        }
+
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                res.push(matrix[i][left])
+            }
+            left++
+        }
+    }
+
+    return res
+};
