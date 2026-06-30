dec30b9182d6fe805bf5c672600145a3a7028660
Homfay-0903 <2790165595@qq.com>
2026-06-30 10:22:03 +0800

    Top-39

---
+++ Top-150/哈希表/39(383).js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+    const charMap = new Map()
+
+    for (const char of magazine) {
+        charMap.set(char, (charMap.get(char) || 0) + 1)
+    }
+
+    for (const char of ransomNote) {
+        if (!charMap.has(char) || !charMap.get(char)) {
+            return false
+        }
+
+        charMap.set(char, charMap.get(char) - 1)
+    }
+
+    return true
+};
