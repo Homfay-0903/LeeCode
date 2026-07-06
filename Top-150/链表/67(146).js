d813125d2e318e9045662b348229624b29b41dd5
Homfay-0903 <2790165595@qq.com>
2026-07-06 11:20:01 +0800

    Top-67

---
+++ Top-150/链表/67(146).js
@@ -0,0 +1,31 @@
+class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity
+        this.cacheMap = new Map()
+    }
+
+    get(key) {
+        if (!this.cacheMap.has(key)) {
+            return -1
+        }
+
+        const cacheValue = this.cacheMap.get(key)
+        this.cacheMap.delete(key)
+        this.cacheMap.set(key, cacheValue)
+
+        return cacheValue
+    }
+
+    put(key, value) {
+        if (this.cacheMap.has(key)) {
+            this.cacheMap.delete(key)
+        } else {
+            if (this.cacheMap.size === this.capacity) {
+                const oldestKey = this.cacheMap.keys().next().value
+                this.cacheMap.delete(oldestKey)
+            }
+        }
+
+        this.cacheMap.set(key, value)
+    }
+}
