58e5b5eed91b06aa3a33b33a2744f4beeaa359d5
Homfay-0903 <2790165595@qq.com>
2026-07-07 14:14:40 +0800

    Top-75

---
+++ Top-150/二叉树/75(114).js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function (root) {
+    while (root) {
+        if (root.left) {
+            let rightestInLeft = root.left
+
+            while (rightestInLeft.right) {
+                rightestInLeft = rightestInLeft.right
+            }
+
+            rightestInLeft.right = root.right
+            root.right = root.left
+            root.left = null
+        }
+
+        root = root.right
+    }
+};
