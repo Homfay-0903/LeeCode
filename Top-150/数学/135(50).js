e7d618e2f5b9dddfdc0593f3fc9bb1bd3a947e15
Homfay-0903 <2790165595@qq.com>
2026-07-22 16:36:43 +0800

    Top-135

---
+++ Top-150/数学/135(50).js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+    if (!n) {
+        return 1
+    }
+
+    if (n < 0) {
+        return 1 / myPow(x, -n)
+    }
+
+    if (n % 2) {
+        return x * myPow(x * x, (n - 1) / 2)
+    } else {
+        return myPow(x * x, n / 2)
+    }
+};
