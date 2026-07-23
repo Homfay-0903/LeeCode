b843031107b41346c6f0990e44927a0e8ad118ad
Homfay-0903 <2790165595@qq.com>
2026-07-23 09:40:58 +0800

    Top-138

---
+++ Top-150/一维动态规划/138(198).js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    const dp = new Array(nums.length + 1).fill(0)
+
+    dp[1] = nums[0]
+    dp[2] = Math.max(nums[0], nums[1])
+
+    for (let i = 3; i <= nums.length; i++) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1])
+    }
+
+    return dp[nums.length]
+};
