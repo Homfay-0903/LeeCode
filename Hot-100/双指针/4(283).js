6ec381325479570a60637f7d111fb599bd767d15
Homfay <2790165595@qq.com>
2026-08-20 17:01:34 +0800

    Hot-4

--- Hot-100/双指针/4(283).js
+++ Hot-100/双指针/4(283).js
@@ -3,17 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-    let slow = 0
-    let fast = 0
-    const len = nums.length
+    const n = nums.length
+    let slow = 0, fast = 0
 
-    for (; fast < len; fast++) {
+    while (fast < n) {
         if (nums[fast] !== 0) {
-            if (fast !== slow) {
-                [nums[fast], nums[slow]] = [nums[slow], nums[fast]]
-            }
+            [nums[fast], nums[slow]] = [nums[slow], nums[fast]]
             slow++
         }
+        fast++
     }
-
 };
