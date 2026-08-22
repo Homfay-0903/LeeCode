563759cfe23a93c988c489dd985cdf98afec1cc4
Homfay-0903 <2790165595@qq.com>
2026-08-22 16:47:34 +0800

    Hot-15

--- Hot-100/普通数组/15(189).js
+++ Hot-100/普通数组/15(189).js
@@ -4,18 +4,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-    if (!k) {
+    if (k === 0) {
         return
     }
 
-    const len = nums.length
-    k = k % len
-    const frontNums = nums.slice(0, len - k)
-    const backNums = nums.slice(len - k)
+    k = k % nums.length
+    const backNums = nums.slice(nums.length - k)
+    const frontNums = nums.slice(0, nums.length - k)
     const newNums = backNums.concat(frontNums)
-
-    //for (let i = 0; i < len; i++) {
-    //    nums[i] = newNums[i]
-    //}
-    nums.splice(0, len, ...newNums)
+    nums.splice(0, nums.length, ...newNums)
 };
