bb9ddf43d0783f3ea28b7f59c34de79898887140
Homfay-0903 <2790165595@qq.com>
2026-08-22 11:22:46 +0800

    Hot-12

--- Hot-100/子串/12(76).js
+++ Hot-100/子串/12(76).js
@@ -4,54 +4,45 @@
  * @return {string}
  */
 var minWindow = function (s, t) {
-    const sLen = s.length
-    const tLen = t.length
-
-    if (sLen < tLen) {
-        return ''
-    }
-
-    const needObj = {}
-    const haveObj = {}
+    const sLen = s.length, tLen = t.length
+    const needObj = {}, haveObj = {}
 
     for (let i = 0; i < tLen; i++) {
         const char = t[i]
-
         needObj[char] = (needObj[char] || 0) + 1
     }
 
+    let strStart = 0
     let curStart = 0, curEnd = 0
-    let charStart = 0
-    let species = 0
-    let charLen = Infinity
-    const needSize = Object.keys(needObj).length
+    let curSpecies = 0, totalSpecies = Object.keys(needObj).length
+    let minLen = Infinity
 
     while (curEnd < sLen) {
         const char = s[curEnd]
-        curEnd++
-
         haveObj[char] = (haveObj[char] || 0) + 1
 
         if (haveObj[char] === needObj[char]) {
-            species++
+            curSpecies++
+
+            while (curSpecies === totalSpecies) {
+                if (curEnd - curStart + 1 < minLen) {
+                    minLen = curEnd - curStart + 1
+                    strStart = curStart
+                }
+
+                const removeChar = s[curStart]
+                curStart++
+
+                if (haveObj[removeChar] === needObj[removeChar]) {
+                    curSpecies--
+                }
+
+                haveObj[removeChar]--
+            }
         }
 
-        while (species === needSize) {
-            if (curEnd - curStart < charLen) {
-                charStart = curStart
-                charLen = curEnd - curStart
-            }
-
-            const removeChar = s[curStart]
-            curStart++
-
-            if (haveObj[removeChar] === needObj[removeChar]) {
-                species--
-            }
-
-            haveObj[removeChar]--
-        }
+        curEnd++
     }
 
-    return charLen === Infinity ? '' : s.substring(charStart, charStart + charLen)
+    return minLen === Infinity ? '' : s.substring(strStart, strStart + minLen)
 };
