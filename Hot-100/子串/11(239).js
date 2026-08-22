3d42026a830056864dc78842b5e67c2da2a9741a
Homfay-0903 <2790165595@qq.com>
2026-08-22 11:03:57 +0800

    Hot-11

--- Hot-100/子串/11(239).js
+++ Hot-100/子串/11(239).js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function (nums, k) {
-    const len = nums.length
+    const n = nums.length
     const queue = []
     const res = []
 
-    for (let i = 0; i < len; i++) {
-        if (queue.length > 0 && queue[0] <= i - k) {
+    for (let i = 0; i < n; i++) {
+        while (queue.length > 0 && queue[0] < i + 1 - k) {
             queue.shift()
         }
 
@@ -19,7 +19,7 @@ var maxSlidingWindow = function (nums, k) {
 
         queue.push(i)
 
-        if (i >= k - 1) {
+        if (i + 1 - k >= 0) {
             res.push(nums[queue[0]])
         }
     }
