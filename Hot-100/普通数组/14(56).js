1c7a04dd0f7b25b70605392f5ff433d9dd125a6f
Homfay-0903 <2790165595@qq.com>
2026-08-22 16:42:33 +0800

    Hot-14

--- Hot-100/普通数组/14(56).js
+++ Hot-100/普通数组/14(56).js
@@ -3,24 +3,18 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-    const len = intervals.length
-    const res = []
-
-    if (!len) {
-        return res
-    }
-
+    const n = intervals.length
     intervals.sort((a, b) => a[0] - b[0])
-    res.push(intervals[0])
+    const res = [intervals[0]]
 
-    for (let i = 1; i < len; i++) {
-        const curArray = intervals[i]
-        const preArray = res[res.length - 1]
+    for (let i = 1; i < n; i++) {
+        const pre = res[res.length - 1]
+        const cur = intervals[i]
 
-        if (curArray[0] <= preArray[1]) {
-            preArray[1] = Math.max(preArray[1], curArray[1])
+        if (pre[1] >= cur[0]) {
+            pre[1] = Math.max(pre[1], cur[1])
         } else {
-            res.push(curArray)
+            res.push(cur)
         }
     }
 
