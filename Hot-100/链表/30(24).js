a95fee406a8890f671a85994b1ca612642b171b7
Homfay <2790165595@qq.com>
2026-08-25 11:09:28 +0800

    Hot-30

--- Hot-100/链表/30(24).js
+++ Hot-100/链表/30(24).js
@@ -10,19 +10,19 @@
  * @return {ListNode}
  */
 var swapPairs = function (head) {
-    const virtualNode = new ListNode(null)
-    let preNode = virtualNode
-    virtualNode.next = head
+    const dummy = new ListNode(0)
+    let preNode = dummy
+    dummy.next = head
 
     while (preNode.next && preNode.next.next) {
         const node1 = preNode.next
         const node2 = preNode.next.next
 
-        preNode.next = node2
         node1.next = node2.next
         node2.next = node1
+        preNode.next = node2
         preNode = node1
     }
 
-    return virtualNode.next
+    return dummy.next
 };
