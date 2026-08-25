1e8ffe19eeb21835b86ce58fc3eac0b9dbafd2f2
Homfay <2790165595@qq.com>
2026-08-25 11:35:28 +0800

    Hot-32

--- Hot-100/链表/32(138).js
+++ Hot-100/链表/32(138).js
@@ -16,23 +16,22 @@ var copyRandomList = function (head) {
         return null
     }
 
-    const nodeMap = new Map()
+    const map = new Map()
     let curNode = head
 
     while (curNode) {
-        nodeMap.set(curNode, new _Node(curNode.val))
+        map.set(curNode, new _Node(curNode.val))
         curNode = curNode.next
     }
 
     curNode = head
 
     while (curNode) {
-        const newNodeList = nodeMap.get(curNode)
-        newNodeList.next = curNode.next ? nodeMap.get(curNode.next) : null
-        newNodeList.random = curNode.random ? nodeMap.get(curNode.random) : null
-
+        const newList = map.get(curNode)
+        newList.next = curNode.next ? map.get(curNode.next) : null
+        newList.random = curNode.random ? map.get(curNode.random) : null
         curNode = curNode.next
     }
 
-    return nodeMap.get(head)
+    return map.get(head)
 };
