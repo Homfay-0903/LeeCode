a0a321a3c0076ecf35e62bcfccf1e143f816992c
Homfay <2790165595@qq.com>
2026-08-25 13:53:03 +0800

    Hot-33

--- Hot-100/链表/33(148).js
+++ Hot-100/链表/33(148).js
@@ -10,45 +10,42 @@
  * @return {ListNode}
  */
 var sortList = function (head) {
+    function mergeTwoList(l1, l2) {
+        const dummy = new ListNode(0)
+        let curNode = dummy
+
+        while (l1 && l2) {
+            if (l1.val < l2.val) {
+                curNode.next = l1
+                l1 = l1.next
+            } else {
+                curNode.next = l2
+                l2 = l2.next
+            }
+
+            curNode = curNode.next
+        }
+
+        curNode.next = l1 ? l1 : l2
+
+        return dummy.next
+    }
+
     if (!head || !head.next) {
         return head
     }
 
-    let slow = head
-    let fast = head
+    let slow = head, fast = head
 
     while (fast.next && fast.next.next) {
         slow = slow.next
         fast = fast.next.next
     }
 
-    const leftList = head
-    const rightList = slow.next
+    const left = head, right = slow.next
     slow.next = null
+    const leftList = sortList(left), rightList = sortList(right)
+    const newList = mergeTwoList(leftList, rightList)
 
-    const leftSort = sortList(leftList)
-    const rightSort = sortList(rightList)
-
-    return mergeTwoList(leftSort, rightSort)
+    return newList
 };
-
-var mergeTwoList = function (l1, l2) {
-    const virtualNode = new ListNode(null)
-    let curNode = virtualNode
-
-    while (l1 && l2) {
-        if (l1.val < l2.val) {
-            curNode.next = l1
-            l1 = l1.next
-        } else {
-            curNode.next = l2
-            l2 = l2.next
-        }
-
-        curNode = curNode.next
-    }
-
-    curNode.next = l1 ? l1 : l2
-
-    return virtualNode.next
-}
