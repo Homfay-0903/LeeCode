310526d1906b8e54bc117c407e0ebb3d5f683dbf
Homfay <2790165595@qq.com>
2026-08-25 18:19:24 +0800

    Hot-40

--- Hot-100/二叉树/40(543).js
+++ Hot-100/二叉树/40(543).js
@@ -13,17 +13,17 @@
 var diameterOfBinaryTree = function (root) {
     let maxMeter = 0
 
-    const caculateMeter = (node) => {
+    function caculateMeter(node) {
         if (!node) {
             return 0
         }
 
-        const leftDepth = caculateMeter(node.left)
-        const rightDepth = caculateMeter(node.right)
+        const leftMeter = caculateMeter(node.left)
+        const rightMeter = caculateMeter(node.right)
 
-        maxMeter = Math.max(maxMeter, leftDepth + rightDepth)
+        maxMeter = Math.max(maxMeter, leftMeter + rightMeter)
 
-        return Math.max(leftDepth, rightDepth) + 1
+        return 1 + Math.max(leftMeter, rightMeter)
     }
 
     caculateMeter(root)
