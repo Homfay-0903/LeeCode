289def5c869fdaf07318143575828fc34458dc5e
Homfay-0903 <2790165595@qq.com>
2026-08-27 16:59:59 +0800

    Hot-60

--- Hot-100/回溯/60(79).js
+++ Hot-100/回溯/60(79).js
@@ -4,31 +4,35 @@
  * @return {boolean}
  */
 var exist = function (board, word) {
-    const rowLength = board.length
-    const colLength = board[0].length
+    const m = board.length
+    const n = board[0].length
+    const wordLen = word.length
 
-    const dfs = (x, y, index) => {
-        if (x < 0 || x >= rowLength || y < 0 || y >= colLength || board[x][y] !== word[index]) {
-            return false
-        }
-        if (index + 1 === word.length) {
+    function backTrack(x, y, idx) {
+        if (idx === wordLen) {
             return true
+        }
+
+        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] !== word[idx]) {
+            return
         }
 
         const curChar = board[x][y]
         board[x][y] = '*'
 
-        const isFound = dfs(x - 1, y, index + 1) || dfs(x + 1, y, index + 1)
-            || dfs(x, y - 1, index + 1) || dfs(x, y + 1, index + 1)
+        const isFound = backTrack(x - 1, y, idx + 1) ||
+            backTrack(x + 1, y, idx + 1) ||
+            backTrack(x, y - 1, idx + 1) ||
+            backTrack(x, y + 1, idx + 1)
 
         board[x][y] = curChar
 
         return isFound
     }
 
-    for (let i = 0; i < rowLength; i++) {
-        for (let j = 0; j < colLength; j++) {
-            if (dfs(i, j, 0)) {
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (backTrack(i, j, 0)) {
                 return true
             }
         }
