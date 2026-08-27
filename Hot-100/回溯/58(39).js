c80cb401d20e1713a1c25d5e6c13c7f9f6b78259
Homfay-0903 <2790165595@qq.com>
2026-08-27 16:36:09 +0800

    Hot-58

--- Hot-100/回溯/58(39).js
+++ Hot-100/回溯/58(39).js
@@ -4,28 +4,27 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
-    const len = candidates.length
+    const n = candidates.length
     const res = []
 
-    candidates = candidates.sort((a, b) => a - b)
+    candidates.sort((a, b) => a - b)
 
-    const backTrack = (startPos, curSum, path) => {
+    function backTrack(curPos, curSum, path) {
         if (curSum === target) {
             res.push([...path])
             return
         }
 
-        for (let i = startPos; i < len; i++) {
+        for (let i = curPos; i < n; i++) {
             const curNum = candidates[i]
 
             if (curNum + curSum > target) {
-                break
+                return
             } else {
                 path.push(curNum)
             }
 
             backTrack(i, curNum + curSum, path)
-
             path.pop()
         }
     }
