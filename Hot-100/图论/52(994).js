d30b3a4c0d25a61fb2d83c53afcaffc5871d7c15
Homfay-0903 <2790165595@qq.com>
2026-08-27 12:10:36 +0800

    Hot-52

--- Hot-100/图论/52(994).js
+++ Hot-100/图论/52(994).js
@@ -5,40 +5,36 @@
 var orangesRotting = function (grid) {
     let time = 0
     let fresh = 0
-
-    const rowLen = grid.length
-    const colLen = grid[0].length
-    const rottenQueue = []
+    const m = grid.length, n = grid[0].length
+    const rotten = []
     const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-    for (let i = 0; i < rowLen; i++) {
-        for (let j = 0; j < colLen; j++) {
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             if (grid[i][j] === 1) {
                 fresh++
             } else if (grid[i][j] === 2) {
-                rottenQueue.push([i, j])
+                rotten.push([i, j])
             }
         }
     }
 
+    while (fresh > 0 && rotten.length > 0) {
+        const curRottenCount = rotten.length
 
-    while (fresh && rottenQueue.length) {
-        const curRottenSize = rottenQueue.length
-
-        for (let i = 0; i < curRottenSize; i++) {
-            const [curRottenFruitX, curRottenFruitY] = rottenQueue.shift()
+        for (let i = 0; i < curRottenCount; i++) {
+            const [x, y] = rotten.shift()
 
             for (const [dx, dy] of dirs) {
-                const newX = dx + curRottenFruitX
-                const newY = dy + curRottenFruitY
+                const newX = x + dx, newY = y + dy
 
-                if (newX < 0 || newX >= rowLen || newY < 0 || newY >= colLen || grid[newX][newY] !== 1) {
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] === 1) {
+                    fresh--
+                    grid[newX][newY] = 2
+                    rotten.push([newX, newY])
+                } else {
                     continue
                 }
-
-                fresh--
-                grid[newX][newY] = 2
-                rottenQueue.push([newX, newY])
             }
         }
 
