3a6378f237e592241b247c0b3c1f7dafe778c686
Homfay-0903 <2790165595@qq.com>
2026-08-27 17:12:00 +0800

    Hot-61

--- Hot-100/回溯/61(131).js
+++ Hot-100/回溯/61(131).js
@@ -5,28 +5,26 @@
 var partition = function (s) {
     const res = []
 
-    const isPalindrome = (left, right) => {
+    function isPalindrome(left, right) {
         while (left < right) {
             if (s[left] !== s[right]) {
                 return false
             }
 
-            left++
-            right--
+            left++, right--
         }
-
         return true
     }
 
-    const backTrack = (startIdx, path) => {
-        if (startIdx === s.length) {
+    function backTrack(pos, path) {
+        if (pos === s.length) {
             res.push([...path])
             return
         }
 
-        for (let i = startIdx; i < s.length; i++) {
-            if (isPalindrome(startIdx, i)) {
-                path.push(s.substring(startIdx, i + 1))
+        for (let i = pos; i < s.length; i++) {
+            if (isPalindrome(pos, i)) {
+                path.push(s.substring(pos, i + 1))
                 backTrack(i + 1, path)
                 path.pop()
             }
