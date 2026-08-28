83c97b94cf24879ccfb67414463c1bb6c80c7b1b
Homfay-0903 <2790165595@qq.com>
2026-08-28 10:42:00 +0800

    Hot-68

--- Hot-100/二分查找/68(4).js
+++ Hot-100/二分查找/68(4).js
@@ -4,18 +4,20 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-    const n1 = nums1.length
-    const n2 = nums2.length
-    const k1 = Math.floor((n1 + n2 + 1) / 2)
-    const k2 = Math.floor((n1 + n2 + 2) / 2)
+    const m = nums1.length
+    const n = nums2.length
+    const k1 = Math.floor((m + n + 1) / 2)
+    const k2 = Math.floor((m + n + 2) / 2)
 
-    const findKth = (arr1, arr2, k) => {
-        if (!arr1.length) {
+    function findKth(arr1, arr2, k) {
+        if (arr1.length === 0) {
             return arr2[k - 1]
         }
-        if (!arr2.length) {
+
+        if (arr2.length === 0) {
             return arr1[k - 1]
         }
+
         if (k === 1) {
             return arr1[0] < arr2[0] ? arr1[0] : arr2[0]
         }
@@ -23,12 +25,11 @@ var findMedianSortedArrays = function (nums1, nums2) {
         const i = Math.min(Math.floor(k / 2), arr1.length) - 1
         const j = Math.min(Math.floor(k / 2), arr2.length) - 1
 
-        if (arr1[i] <= arr2[j]) {
+        if (arr1[i] < arr2[j]) {
             return findKth(arr1.slice(i + 1), arr2, k - (i + 1))
         } else {
             return findKth(arr1, arr2.slice(j + 1), k - (j + 1))
         }
-
     }
 
     return (findKth(nums1, nums2, k1) + findKth(nums1, nums2, k2)) / 2
