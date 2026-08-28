078da821a1f5831afe2039b1cb43de58d23cc349
Homfay-0903 <2790165595@qq.com>
2026-08-28 10:12:43 +0800

    Hot-64

--- Hot-100/二分查找/64(74).js
+++ Hot-100/二分查找/64(74).js
@@ -4,20 +4,18 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-    const rowLen = matrix.length
-    const colLen = matrix[0].length
-
-    let left = 0
-    let right = rowLen * colLen - 1
+    const m = matrix.length
+    const n = matrix[0].length
+    let left = 0, right = m * n - 1
 
     while (left <= right) {
         const mid = Math.floor((left + right) / 2)
-        const curRow = Math.floor(mid / colLen)
-        const curCol = mid % colLen
+        const rowIdx = Math.floor(mid / n)
+        const colIdx = mid % n
 
-        if (matrix[curRow][curCol] === target) {
+        if (matrix[rowIdx][colIdx] === target) {
             return true
-        } else if (matrix[curRow][curCol] < target) {
+        } else if (matrix[rowIdx][colIdx] < target) {
             left = mid + 1
         } else {
             right = mid - 1
