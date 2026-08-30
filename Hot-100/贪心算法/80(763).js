8f1595888b5f6a3f1bac3f63f214022b0bd4639e
Homfay-0903 <2790165595@qq.com>
2026-08-30 15:47:40 +0800

    Hot-80

--- Hot-100/贪心算法/80(763).js
+++ Hot-100/贪心算法/80(763).js
@@ -3,20 +3,20 @@
  * @return {number[]}
  */
 var partitionLabels = function (s) {
-    const len = s.length
+    const n = s.length
+    const res = []
     const last = new Array(26).fill(0)
 
-    for (let i = 0; i < len; i++) {
-        const charIndex = s.charCodeAt(i) - 'a'.charCodeAt(0)
-        last[charIndex] = i
+    for (let i = 0; i < n; i++) {
+        const charIdx = s.charCodeAt(i) - 'a'.charCodeAt(0)
+        last[charIdx] = i
     }
 
-    const res = []
     let start = 0, end = 0
-
-    for (let i = 0; i < len; i++) {
-        const charIndex = s.charCodeAt(i) - 'a'.charCodeAt(0)
-        end = Math.max(end, last[charIndex])
+    for (let i = 0; i < n; i++) {
+        const charIdx = s.charCodeAt(i) - 'a'.charCodeAt(0)
+        const lastIdx = last[charIdx]
+        end = Math.max(end, lastIdx)
 
         if (i === end) {
             res.push(end - start + 1)
