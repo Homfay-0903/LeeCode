23181c0b2890bd239ef7c6fd5290d2e5fd15a17a
Homfay-0903 <2790165595@qq.com>
2026-08-31 12:11:56 +0800

    Hot-82

--- Hot-100/动态规划/82(118).js
+++ Hot-100/动态规划/82(118).js
@@ -6,11 +6,11 @@ var generate = function (numRows) {
     const res = [[1]]
 
     for (let i = 1; i < numRows; i++) {
-        const preRow = res[i - 1]
+        const preRow = res[res.length - 1]
         const curRow = [1]
 
         for (let j = 1; j < i; j++) {
-            curRow[j] = preRow[j - 1] + preRow[j]
+            curRow[j] = preRow[j] + preRow[j - 1]
         }
 
         curRow.push(1)
