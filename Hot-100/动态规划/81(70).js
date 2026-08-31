52ddf67592a62841210a47a841f265d8958d6cbb
Homfay-0903 <2790165595@qq.com>
2026-08-31 11:54:26 +0800

    Hot-81

--- Hot-100/动态规划/81(70).js
+++ Hot-100/动态规划/81(70).js
@@ -3,15 +3,10 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-    if (n === 1) {
-        return 1
-    }
-
     const dp = new Array(n + 1).fill(0)
-    dp[1] = 1
-    dp[2] = 2
+    dp[1] = 1, dp[2] = 2
 
-    for (let i = 3; i <= n; i++) {
+    for (let i = 3; i < n + 1; i++) {
         dp[i] = dp[i - 1] + dp[i - 2]
     }
 
