d88f7f757d8539e8f627d1b6fb33e85addf84845
Homfay-0903 <2790165595@qq.com>
2026-08-31 12:28:24 +0800

    Hot-84

--- Hot-100/动态规划/84(279).js
+++ Hot-100/动态规划/84(279).js
@@ -4,10 +4,10 @@
  */
 var numSquares = function (n) {
     const dp = new Array(n + 1).fill(Infinity)
-    dp[0] = 0
-    dp[1] = 1
 
-    for (let i = 2; i <= n; i++) {
+    dp[0] = 0
+
+    for (let i = 1; i <= n; i++) {
         for (let j = 1; j * j <= i; j++) {
             dp[i] = Math.min(dp[i], dp[i - j * j] + 1)
         }
