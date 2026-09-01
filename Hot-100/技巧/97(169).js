d345907f7296ce67fbdff5c462f7818fe1f9f43e
Homfay-0903 <2790165595@qq.com>
2026-09-01 16:03:02 +0800

    Hot-97

--- Hot-100/技巧/97(169).js
+++ Hot-100/技巧/97(169).js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    const len = nums.length
-    const numMap = new Map()
+    const map = new Map()
 
     for (const num of nums) {
-        numMap.set(num, (numMap.get(num) || 0) + 1)
+        map.set(num, (map.get(num) || 0) + 1)
 
-        if (numMap.get(num) > (len / 2)) {
+        if (map.get(num) > nums.length / 2) {
             return num
         }
     }
