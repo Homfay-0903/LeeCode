c2e4afe5071884f4ea3ef435c9d1839a81706b86
Homfay-0903 <2790165595@qq.com>
2026-09-01 09:54:57 +0800

    Hot-92

--- Hot-100/多维动态规划/92(64).js
+++ Hot-100/多维动态规划/92(64).js
@@ -5,12 +5,14 @@
 var minPathSum = function (grid) {
     const m = grid.length
     const n = grid[0].length
-    const dp = new Array(m).fill(null).map(() => new Array(n).fill(null))
+    const dp = new Array(m).fill(0).map(() => new Array(n).fill(0))
+
     dp[0][0] = grid[0][0]
 
     for (let i = 1; i < m; i++) {
         dp[i][0] = dp[i - 1][0] + grid[i][0]
     }
+
     for (let i = 1; i < n; i++) {
         dp[0][i] = dp[0][i - 1] + grid[0][i]
     }
